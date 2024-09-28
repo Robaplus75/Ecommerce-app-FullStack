06df911dfe63965e0d3c4f15aed5b8b0f5a6ca08
robel <robelwon75@gmail.com>
2024-09-29 00:34:58 +0300

    added a route

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Home from "./pages/Home"
 import Shop from "./pages/Shop"
 import Cart from "./pages/Cart"
 import Checkout from "./pages/Checkout"
+import Order from "./pages/Order"
 
 const router = createBrowserRouter([
   {
@@ -24,6 +25,10 @@ const router = createBrowserRouter([
       {
         path:"/checkout",
         element: <Checkout />
+      },
+      {
+        path:"order-confirmation",
+        element: <Order />,
       }
     ]
   },
