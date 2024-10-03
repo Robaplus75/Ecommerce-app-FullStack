475e2e261bda290857ec115f6b3c3c0638c3a51a
robel <robelwon75@gmail.com>
2024-10-03 12:56:09 +0300

    changed

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from .CustomUserManager import CustomUserManager
+import uuid
 # Create your models here.
 
 class CustomUser(AbstractUser):
+	id_uuid = models.UUIDField(primary_key=True, default=uuid.uuid4())
 	username = models.CharField(
         "username",
         max_length=150,
