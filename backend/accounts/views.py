7a0db6301abf56a67472ec834b0d97f86a99ab77
robel <robelwon75@gmail.com>
2024-10-04 22:29:30 +0300

    created a user create api

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from rest_framework import generics
+from .serializers import UserSerializer
+from django.contrib.auth import get_user_model
 # Create your views here.
+
+User = get_user_model()
+
+class UserCreateAPIView(generics.CreateAPIView):
+	serializer_class = UserSerializer
+	queryset = User.objects.all()
