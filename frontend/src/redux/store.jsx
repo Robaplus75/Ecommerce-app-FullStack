408b614deab76536f6c788c1136fc90ea5358180
robel <robelwon75@gmail.com>
2024-10-06 11:51:36 +0300

    added userSlice

--- frontend/src/redux/store.jsx
+++ frontend/src/redux/store.jsx
@@ -2,12 +2,14 @@ import {configureStore} from '@reduxjs/toolkit'
 import productReducer from './productSlice'
 import cartReducer from './cartSlice'
 import orderReducer from './orderSlice'
+import userReducer from './userSlice'
 
 const store = configureStore({
 	reducer: {
 		cart: cartReducer,
 		product: productReducer,
-		order: orderReducer
+		order: orderReducer,
+		user: userReducer,
 	}
 })
 
