8d4cc1be5dc2bec55bc8c5ba4f05184db37f27fa
robel <robelwon75@gmail.com>
2024-10-06 11:58:11 +0300

    untracked commit

--- backend/products/views.py
+++ backend/products/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .serializers import ProductSerialzier
+from .models import Product
 
-# Create your views here.
+class ProductListView(generics.ListAPIView):
+	serializer_class = ProductSerialzier
+	queryset = Product.objects.all()
+
+
+class ProductCreateView(generics.CreateAPIView):
+	serializer_class = ProductSerialzier
+	queryset = Product.objects.all()
