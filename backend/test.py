dac16347ac1f523590bbb28ef6569e22e4439ff4
robel <robelwon75@gmail.com>
2024-10-08 19:28:15 +0300

    test file updated

--- backend/test.py
+++ backend/test.py
@@ -7,6 +7,8 @@ endpoint = "http://localhost:8000/products/create/"
 data = {"name":"Bag", "price":24.00}
 file = {"image":image_file}
 
+print("sending Post request")
+
 get_response = requests.post(endpoint, data=data, files=file)
 
 print(get_response.json())
