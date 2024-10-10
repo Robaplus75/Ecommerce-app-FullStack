74ee1368ca4381d62841380118f733fbb7a33720
robel <robelwon75@gmail.com>
2024-10-10 14:38:57 +0300

    added a functionainilty that checks if user is logged in

--- frontend/src/components/Container.jsx
+++ frontend/src/components/Container.jsx
@@ -2,8 +2,22 @@ import {Outlet} from 'react-router-dom'
 import Footer from './Footer'
 import Navbar from './Navbar'
 import {Toaster} from 'react-hot-toast'
+import {useEffect} from 'react'
+import {login_If_TokenPresent} from '../redux/userSlice'
+import {useDispatch} from 'react-redux'
+import toast from 'react-hot-toast'
 
 export default function Container(){
+	const dispatch = useDispatch()
+	async function check_if_logged(){
+		const res = await dispatch(login_If_TokenPresent())
+		if (res.type==="Login_If_TokenPresent/fulfilled"){
+			toast.success("User Already Logged in")
+		}
+	}
+	useEffect(()=>{
+		check_if_logged()
+	},[])
 	return (
 		<div>
 			<Toaster />
