3f23fcfdaacd6ab0f6a6eaa3a2ed81179b7bf516
robel <robelwon75@gmail.com>
2024-10-10 14:40:30 +0300

    added a new route

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -3,8 +3,8 @@ from rest_framework_simplejwt.views import TokenRefreshView, TokenObtainPairView
 from . import views
 
 urlpatterns = [
+    path("accounts/me/", views.UserRetriveAPIView.as_view()),
     path("accounts/", views.UserCreateAPIView.as_view()),
     path("accounts/token/refresh/", TokenRefreshView.as_view()),
     path("accounts/token/", TokenObtainPairView.as_view()),
-    path("accounts/<str:email>", views.UserRetriveAPIView.as_view()),
 ]
