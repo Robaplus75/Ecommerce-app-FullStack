4907886388cf69de867850e27dad483f559ff430
robel <robelwon75@gmail.com>
2024-10-10 14:40:05 +0300

    added a new view

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, status
 from .serializers import UserSerializer
 from django.contrib.auth import get_user_model
 from .permissions import Is_object_owner
+from rest_framework.views import APIView
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework.response import Response
 # Create your views here.
 
 User = get_user_model()
@@ -11,8 +14,11 @@ class UserCreateAPIView(generics.CreateAPIView):
 	serializer_class = UserSerializer
 	queryset = User.objects.all()
 
-class UserRetriveAPIView(generics.RetrieveAPIView):
+class UserRetriveAPIView(APIView):
 	serializer_class = UserSerializer
-	queryset = User.objects.all()
-	lookup_field = 'email'
-	permission_classes = [Is_object_owner]
+	permission_classes = [permissions.IsAuthenticated]
+
+	def get(self, request):
+		user = request.user
+		serializer = self.serializer_class(user)
+		return Response(serializer.data)
